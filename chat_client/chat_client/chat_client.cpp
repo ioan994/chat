dbc36e2e3fc1df5683c92d7f4a35b292b1752214
Ivan Petrenko <ioan.petrenko@gmail.com>
2016-08-31 12:52:49 +0200

    .

--- chat_client/chat_client/chat_client.cpp
+++ chat_client/chat_client/chat_client.cpp
@@ -17,7 +17,8 @@
 
 using namespace boost::asio;
 
-//static const std::string ipAddr = "127.0.0.1";
+//static const std::string defaultAddr = "127.0.0.1";
+static const std::string defaultAddr = "92.249.111.144";
 static const std::string ipAddr = "10.30.1.37";
 //static const std::string ipAddr = "10.30.1.18";
 
@@ -27,12 +28,22 @@ int _tmain(int argc, _TCHAR* argv[])
 {
    const int requests = 5;
    
+   std::string address = argc <= 1 ? defaultAddr : std::string((char*)(argv[1]));
+
    //requestTime(portNumber);
 
-   console_ui cli;
-   client client(cli, ip::tcp::endpoint(ip::address::from_string(ipAddr), portNumber));
-   cli.set_chat_client(&client);
-   cli.start_listening_keystrokes();
+   try
+   {
+      console_ui cli;
+      client client(cli, ip::tcp::endpoint(ip::address::from_string(address), portNumber));
+      cli.set_chat_client(&client);
+      cli.start_listening_keystrokes();
+   }
+   catch (std::exception& e)
+   {
+      std::cout << "Something went wrong: " << e.what();
+   }
+   std::cin.get();
 
 	return 0;
 }
